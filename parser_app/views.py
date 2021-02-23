33a0dbef1f7a19ac8e8ab93ee262cf9c170f81b1
niksoluv <vitalik.blablabla00@gmail.com>
2021-02-23 18:19:24 +0200

    post request receiveing

--- parser_app/views.py
+++ parser_app/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 
 # import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 
 # def IndexView(request):
 # 	return HttpResponse("iosdjafiosajfio")
@@ -9,8 +9,15 @@ from django.http import HttpResponse
 from rest_framework import views
 from rest_framework.response import Response
 from .serializers import ParseSerializer
+import json
 class IndexView(views.APIView):
-    def get(self, request):
-        yourdata= [{"url": 'url1', "html": 'http1'}, {"url": 'url2', "html": 'http2'}]
-        results = ParseSerializer(yourdata, many=True).data
-        return Response(results)
+	def get(self, request):
+		yourdata= [{"url": 'url1', "html": 'http1'}, {"url": 'url2', "html": 'http2'}]
+		results = ParseSerializer(yourdata, many=True).data
+		return Response(results)
+	
+	def post(self, request):
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		print(body)
+		return Response(request.path)
