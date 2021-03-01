fe826c31ebb38861c21bf476883fdd2f9edd944a
Dmytro Dudyk <dmytro.dudyk.ds@gmail.com>
2021-03-01 14:18:33 +0200

    added Parser packege with BaseParser module

---
+++ Parser/LandingParser.py
@@ -0,0 +1,13 @@
+from Parser import BaseParser
+
+
+class LandingParser(BaseParser):
+
+    def __init__(self, html_code):
+        super().__init__(html_code)
+
+    def get_list_of_groupd_first_last_names(self):
+        pass
+
+    def get_list_of_links_to_images(self):
+        pass
